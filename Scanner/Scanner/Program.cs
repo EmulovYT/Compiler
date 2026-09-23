25b3ded82d8a68053af79e8d6f07f8a0eb1285ca
Студент <@65-6>
2026-09-23 13:32:48 +0300

    Добавлен проект WordCounter

---
+++ Scanner/Scanner/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace WordCounter 
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Введите текст:");
+            string text = Console.ReadLine();
+
+            char[] separators = new char[]
+            {
+                ' ', '\t', '\n', '\r',
+                ',', '.', '!', '?', ';', ':', '-', '—'
+            };
+
+            string[] words = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine($"Количество слов: {words.Length}");
+
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
